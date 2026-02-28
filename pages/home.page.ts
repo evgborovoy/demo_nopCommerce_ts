e982c11c30948c0ce0ab3a52707db5212cfe0496
Evgeniy_Borovoy <borovoyevgen@gmail.com>
2026-02-28 20:19:03 +0100

    add search method on home page

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -1,6 +1,7 @@
 import type {Page, Locator} from "@playwright/test"
 import {BasePage} from "./base.page.js"
 import {HeaderComponent} from "./components/header.component.js";
+import {SearchResultPage} from "./search_result.page.js";
 
 
 export class HomePage extends BasePage {
@@ -21,6 +22,20 @@ export class HomePage extends BasePage {
     async open(): Promise<void> {
         await super.open();
         await this.searchInput.waitFor({state: 'visible'});
+    }
+
+
+    async search(text: string): Promise<SearchResultPage> {
+        await this.searchInput.fill(text);
+        await this.searchButton.click();
+
+        await Promise.all([
+            this.page.waitForURL("**/search**")
+        ])
+
+        const searchResult = new SearchResultPage(this.page)
+        await searchResult.waitForLoaded()
+        return searchResult
     }
 
 }
